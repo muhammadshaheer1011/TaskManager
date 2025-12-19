9712fb58106bb5df34d6df5cbad33f25cad4ef76
k228719 <k228719@nu.edu.pk>
2025-12-19 17:47:05 +0500

    Revert "using cloudinary method for express"
    This reverts commit 870b85490bc8b6f06a321b123c7118aed516fb1f.

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,22 +1,13 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema(
-  {
-    title: String,
-    description: String,
-    dueDate: Date,
-    status: {
-      type: String,
-      enum: ['pending', 'completed'],
-      default: 'pending',
-    },
-    image: String, // ✅ Cloudinary URL
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-    },
-  },
-  { timestamps: true }
-);
+
+const taskSchema = new mongoose.Schema({
+title: String,
+description: String,
+dueDate: Date,
+status: String,
+user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
 
 module.exports = mongoose.model('Task', taskSchema);
